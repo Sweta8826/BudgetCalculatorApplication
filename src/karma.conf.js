b59407c6b0ecba1899d76a3731bbf04a4ac0859c
Sweta8826 <63239623+Sweta8826@users.noreply.github.com>
2020-09-27 21:14:30 +0530

    Update karma.conf.js

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -9,23 +9,24 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-junit-reporter'),
+      
       require('@angular-devkit/build-angular/plugins/karma')
     ],
-    client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    reporters: ['junit'],    
+    junitReporter: { 
+ outputDir: 'karma-results',    
+ outputFile: 'karma-results.xml'    
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+    angularCli: {
+      environment: 'dev',
     },
-    reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
+    autoWatch: false, 
     singleRun: false
   });
 };
